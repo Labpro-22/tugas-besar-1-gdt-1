807f9eb66d8c13ef26afc08e83121c034d20f25d
Alpaomega1136 <annayuliati69@gmail.com>
2026-04-15 13:59:23 +0700

    Add : Class DiceManager into Models

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -28,3 +28,37 @@ int main(){
 
     return 0;
 }
+
+
+// Kelas untuk mengelola dadu pada permainan 
+class DiceManager{ 
+    private : 
+        int die1;
+        int die2;
+    public :
+    DiceManager(){}
+        std::pair<int, int> rollRandom(){
+            die1 = rand() % 6 + 1;
+            die2 = rand() % 6 + 1;
+            return {die1, die2};
+        }
+        int getDie1() const { return die1; }
+        int getDie2() const { return die2; }
+        int getTotal() const { return die1 + die2; }
+        bool isDouble() const { return die1 == die2; }
+};
+
+
+
+// Kelas Game untuk sebagai objek yang ada selama permainan berlangsung, mengelola logika permainan, dan berinteraksi dengan pengguna melalui GUI.
+class Game{ // menunggu kelas objek jadi
+    private :
+    public :
+};
+
+class GameEngine{
+    private :
+        DiceManager dices;
+    public :
+};
+

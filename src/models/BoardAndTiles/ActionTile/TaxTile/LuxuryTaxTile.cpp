bbe679ba33251fe6d13a9710411678cf84cbbae9
BenedictD-RH <benedictdarrel572006@gmail.com>
2026-04-21 18:40:28 +0700

    feat: added getter methods

--- src/models/BoardAndTiles/ActionTile/TaxTile/LuxuryTaxTile.cpp
+++ src/models/BoardAndTiles/ActionTile/TaxTile/LuxuryTaxTile.cpp
@@ -5,6 +5,10 @@ LuxuryTaxTile::LuxuryTaxTile(int index, int flatAmount)
     : TaxTile(index, "PBM", "Pajak Barang Mewah"),
       flatAmount(flatAmount) {}
 
+const int LuxuryTaxTile::getFlatAmount() const {
+    return flatAmount;
+}
+
 int LuxuryTaxTile::calculateTax(const Player &player) const
 {
     return flatAmount;

c057475f1fc143e587e038ab5372d626622e8746
BenedictD-RH <benedictdarrel572006@gmail.com>
2026-04-22 00:22:39 +0700

    feat: implemented board tile icons

--- src/views/viewElement/board/BoardView.cpp
+++ src/views/viewElement/board/BoardView.cpp
@@ -4,14 +4,13 @@
 
 void getBaseBoardTexture(RenderTexture2D* texture, int width, int height) {
     BeginTextureMode(*texture);
-
+    ClearBackground({205, 230, 208, 255});
     Image img = LoadImage("data/GUIAssets/nimonspoli_logo.png");
     ImageResize(&img, img.width*1.1, img.height*1.1);
     ImageFlipHorizontal(&img);
     ImageRotate(&img, 45);
     Texture logo = LoadTextureFromImage(img);
     UnloadImage(img);
-    DrawRectangle(0,0,width,height, LIGHTGRAY);
     DrawTexture(logo, (width - logo.width)/2, (height-logo.height)/2, WHITE);
 
     img = LoadImage("data/GUIAssets/chance_base.png");
@@ -49,11 +48,11 @@ BoardView::BoardView(Board& board) : View3D({0,0.015,0}, getBoardModel(board), W
     getBaseBoardTexture(&texture, 1500, 1500);
     model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture.texture;
     
-    int i = 0;
+    int i = 9;
     int tilePerDirection = board.getAllTiles().size() / 4;
     float boardSide = (tilePerDirection - 1)*TileView::getTileDim().x + 2*TileView::getTileDim().y;
-    Vector3 startingPos = {(boardSide - TileView::getTileDim().y)/2, 0.03, (boardSide - TileView::getTileDim().y)/2};
-    Vector3 translationNorm = {1, 0, 0};
+    Vector3 startingPos = {-(boardSide - TileView::getTileDim().y)/2, 0.03, (boardSide - TileView::getTileDim().y)/2};
+    Vector3 translationNorm = {-1, 0, 0};
     Matrix tileRotation = MatrixRotate({0,1,0}, 0);
     for (Tile* tile : board.getAllTiles()) {
         TileView* tileView;
@@ -85,22 +84,22 @@ BoardView::BoardView(Board& board) : View3D({0,0.015,0}, getBoardModel(board), W
             } else if (tile->getCode() == "PEN") {
                 tileView = new JailTileView(*tile, isCorner);
             } else if (tile->getCode() == "BBP") {
-                tileView = new TileView(*tile, "FREE", "PARKING", isCorner, "data/GUIAssets/freeparking_icon");
+                tileView = new TileView(*tile, "FREE", "PARKING", isCorner, "data/GUIAssets/freeparking_icon.png");
             } else if (tile->getCode() == "PPJ") {
-                tileView = new TileView(*tile, "GO TO", "JAIL", isCorner, "data/GUIAssets/gotojail_icon");
+                tileView = new TileView(*tile, "GO TO", "JAIL", isCorner, "data/GUIAssets/gotojail_icon.png");
             } else if (tile->getCode() == "KSP") {
-                tileView = new TileView(*tile, "CHANCE", "", isCorner, "data/GUIAssets/chancetile_icon");
+                tileView = new TileView(*tile, "CHANCE", "", isCorner, "data/GUIAssets/chancetile_icon.png");
             } else if (tile->getCode() == "DNU") {
-                tileView = new TileView(*tile, "COMMUNITY CHEST", "", isCorner, "data/GUIAssets/comchesttile_icon");
+                tileView = new TileView(*tile, "COMMUNITY CHEST", "", isCorner, "data/GUIAssets/comchesttile_icon.png");
             } else if (tile->getCode() == "FES") {
-                tileView = new TileView(*tile, "FESTIVAL", "", isCorner, "data/GUIAssets/festival_icon");
+                tileView = new TileView(*tile, "FESTIVAL", "", isCorner, "data/GUIAssets/festival_icon.png");
             }
         }
         tileView->setPos(startingPos);
-        tileView->transform(tileRotation);
+        
         
 
-        Matrix rotation = MatrixRotate({0,1,0}, M_PI/2);
+        Matrix rotation = MatrixRotate({0,1,0}, -M_PI/2);
         if (isCorner) {
             translationNorm = Vector3Transform(translationNorm, rotation);
             tileRotation = MatrixMultiply(rotation, tileRotation);
@@ -112,9 +111,14 @@ BoardView::BoardView(Board& board) : View3D({0,0.015,0}, getBoardModel(board), W
             startingPos += translationNorm*TileView::getTileDim().x;
         }
         i++;
+        tileView->transform(tileRotation);
         tiles.push_back(*tileView);
     }
     cout<<tiles.size()<<endl;
+}
+
+const float BoardView::getBoardSize() const {
+    return GetModelBoundingBox(model).max.x - GetModelBoundingBox(model).min.x;
 }
 
 void BoardView::render() {
--- src/views/viewElement/board/TileView.cpp
+++ src/views/viewElement/board/TileView.cpp
@@ -62,6 +62,7 @@ RenderTexture2D getBaseTileTexture(bool isCorner, string header, string footer, 
     if(!isCorner) {
         RenderTexture2D texture = LoadRenderTexture(TileView::getTileDim().x*100, TileView::getTileDim().y*100);
         BeginTextureMode(texture);
+        ClearBackground({205, 230, 208, 255});
         if (hasColor) {
             DrawRectangle(0,TileView::getTileDim().y*80, TileView::getTileDim().x*100, TileView::getTileDim().y*20, tileColor);
             DrawLineEx({0, TileView::getTileDim().y*80}, {TileView::getTileDim().x*100, TileView::getTileDim().y*80},
@@ -76,10 +77,10 @@ RenderTexture2D getBaseTileTexture(bool isCorner, string header, string footer, 
         if(hasColor) {
             headerSpace += 15;
         }
-        drawTextLinesWrapped(View2D::getFont("Orbitron"), header, 
-                             {TileView::getTileDim().x*50, TileView::getTileDim().y*headerSpace}, 36 , 1, BLACK, TileView::getTileDim()*100);
-        drawTextLinesWrapped(View2D::getFont("Orbitron"), footer, 
-                             {TileView::getTileDim().x*50, TileView::getTileDim().y*90}, 36 , 1, BLACK, TileView::getTileDim()*100);
+        drawTextLinesWrapped(View2D::getFont("Kabel"), header, 
+                             {TileView::getTileDim().x*50, TileView::getTileDim().y*headerSpace}, 32 , 1, BLACK, TileView::getTileDim()*100);
+        drawTextLinesWrapped(View2D::getFont("Kabel"), footer, 
+                             {TileView::getTileDim().x*50, TileView::getTileDim().y*90}, 32 , 1, BLACK, TileView::getTileDim()*100);
         EndTextureMode();
         
         Image textImg = LoadImageFromTexture(text.texture);
@@ -96,10 +97,11 @@ RenderTexture2D getBaseTileTexture(bool isCorner, string header, string footer, 
     } else {
         RenderTexture2D texture = LoadRenderTexture(TileView::getTileDim().y*100, TileView::getTileDim().y*100);
         BeginTextureMode(texture);
+        ClearBackground({205, 230, 208, 255});
         if (hasColor) {
-            DrawRectangle(0,TileView::getTileDim().y*70, TileView::getTileDim().y*30, TileView::getTileDim().y*30, tileColor);
-            DrawRectangleLinesEx({0,TileView::getTileDim().y*70, 
-                                 TileView::getTileDim().y*30, TileView::getTileDim().y*30}, 6, BLACK);
+            DrawRectangle(0,TileView::getTileDim().y*60, TileView::getTileDim().y*40, TileView::getTileDim().y*40, tileColor);
+            DrawRectangleLinesEx({0,TileView::getTileDim().y*60, 
+                                 TileView::getTileDim().y*40, TileView::getTileDim().y*40}, 6, BLACK);
         }
         DrawRectangleLinesEx({0,0, TileView::getTileDim().y*100, TileView::getTileDim().y*100}, 6, BLACK);
         EndTextureMode();
@@ -109,12 +111,12 @@ RenderTexture2D getBaseTileTexture(bool isCorner, string header, string footer, 
         BeginTextureMode(text);
         int headerSpace = 15;
         if(hasColor) {
-            headerSpace += 15;
+            headerSpace += 30;
         }
-        drawTextLinesWrapped(View2D::getFont("Orbitron"), header, 
-                             {textBB.x/2, textBB.y*headerSpace/100}, 36 , 1, BLACK, textBB);
-        drawTextLinesWrapped(View2D::getFont("Orbitron"), footer, 
-                             {textBB.x/2, textBB.y*0.9f}, 36 , 1, BLACK, textBB);
+        drawTextLinesWrapped(View2D::getFont("Kabel"), header, 
+                             {textBB.x/2, textBB.y*headerSpace/100}, 32 , 1, BLACK, textBB);
+        drawTextLinesWrapped(View2D::getFont("Kabel"), footer, 
+                             {textBB.x/2, textBB.y*0.9f}, 32 , 1, BLACK, textBB);
         EndTextureMode();
         
         Image textImg = LoadImageFromTexture(text.texture);
@@ -131,11 +133,43 @@ RenderTexture2D getBaseTileTexture(bool isCorner, string header, string footer, 
     }
 }
 
+void drawImageOnTexture(RenderTexture2D* texture, string filePath, bool isCorner) {
+    Image img = LoadImage(filePath.c_str());
+    ImageFlipVertical(&img);
+    if (img.width > texture->texture.width*0.6) {
+        ImageResize(&img, texture->texture.width*0.6, img.height*(texture->texture.width*0.6/img.width));
+    } 
+    if (img.height > texture->texture.height*0.5) {
+        ImageResize(&img, img.width*(texture->texture.height*0.5/img.height), texture->texture.height*0.5);
+    }
+    if (isCorner) {
+        ImageRotate(&img, 45);
+    }
+    Texture imgTexture = LoadTextureFromImage(img);
+    
+    BeginTextureMode(*texture);
+    DrawTexture(imgTexture, (texture->texture.width - img.width)/2, (texture->texture.height - img.height)/2, WHITE);
+    EndTextureMode();
+    UnloadImage(img);
+    UnloadTexture(imgTexture);
+}
+
 TileView::TileView(Tile& tile, const string tileHeader, const string tileFooter, const bool cornerTile, const string iconFilePath) :  
-    View3D(View3D({0,0}, getTileModel(cornerTile), WHITE)), tile(tile), 
+    View3D(View3D({0,0}, getTileModel(cornerTile), WHITE)), tile(tile), isTextureLoaded(false),
     tileHeader(tileHeader), tileFooter(tileFooter), cornerTile(cornerTile), 
     tileTexture(getBaseTileTexture(cornerTile, getSpacedString(tileHeader), getSpacedString(tileFooter), tile)) {
-    model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = tileTexture.texture;
+    if (iconFilePath != "") {
+        drawImageOnTexture(&tileTexture, iconFilePath, cornerTile);
+    }
+}
+
+void TileView::render() {
+    if (!isTextureLoaded) {
+        model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = tileTexture.texture;
+        isTextureLoaded = true;
+    }
+    model.transform = transformation;
+    DrawModel(model, pos, 1, color);
 }
 
 const Vector2 TileView::getTileDim() {
@@ -146,9 +180,9 @@ PropertyTileView::PropertyTileView(PropertyTile& tile, const bool cornerTile) :
     TileView(tile, tile.getProperty()->getName(), "M" + to_string(tile.getProperty()->getPurchasePrice()), 
              cornerTile, ""), property(*tile.getProperty()) {
     if (tile.getProperty()->getType() == PropertyType::RAILROAD) {
-        
+        drawImageOnTexture(&tileTexture, "data/GUIAssets/railroad_icon.png", cornerTile);
     } else if (tile.getProperty()->getType() == PropertyType::UTILITY) {
-
+        drawImageOnTexture(&tileTexture, "data/GUIAssets/utility"+ tile.getCode() + "_icon.png", cornerTile);
     }
 }
 
@@ -157,7 +191,130 @@ StreetTileView::StreetTileView(PropertyTile& tile, StreetProperty& street, const
 
 
 GoTileView::GoTileView(GoTile& tile, const bool cornerTile) : 
-    TileView(tile, "COLLECT M" + to_string(tile.getSalary()) + " SALARY", "", cornerTile, "data/GUIAssets/go_icon.png") {}
+    TileView(tile, "COLLECT M" + to_string(tile.getSalary()) + " SALARY", "", cornerTile, "data/GUIAssets/go_icon.png") {
+    BeginTextureMode(tileTexture);
+    Image img = LoadImage("data/GUIAssets/goarrow_icon.png");
+    ImageFlipVertical(&img);
+    if (img.width > tileTexture.texture.width*0.6) {
+        ImageResize(&img, tileTexture.texture.width*0.6, img.height*(tileTexture.texture.width*0.6/img.width));
+    }
+    Texture2D imgTexture = LoadTextureFromImage(img);
+    
+    DrawTexture(imgTexture, (tileTexture.texture.width - img.width)/2, tileTexture.texture.height*0.02, WHITE);
+    EndDrawing();
+    UnloadImage(img);
+    UnloadTexture(imgTexture);
+}
 
 JailTileView::JailTileView(Tile& tile, const bool cornerTile) :
-    TileView(tile, "", "JUST VISITING", cornerTile, "data/GUIAssets/jail_icon.png") {}
+    TileView(tile, "IN JAIL", "JUST VISITING", cornerTile, "") {
+    BeginTextureMode(tileTexture);
+    ClearBackground({205, 230, 208, 255});
+    if(!cornerTile) {
+        DrawRectangle(0,TileView::getTileDim().y*40, TileView::getTileDim().x*100, TileView::getTileDim().y*60, ORANGE);
+        DrawRectangleLinesEx({0,TileView::getTileDim().y*40, TileView::getTileDim().x*100, TileView::getTileDim().y*60},6, BLACK);
+        DrawRectangleLinesEx({0,0,TileView::getTileDim().y*100, TileView::getTileDim().y*100}, 6, BLACK);
+        EndTextureMode();
+
+        RenderTexture2D text = LoadRenderTexture(TileView::getTileDim().x*100, TileView::getTileDim().y*100);
+        BeginTextureMode(text);
+
+        drawTextLinesWrapped(View2D::getFont("Kabel"), tileHeader, 
+                             {TileView::getTileDim().x*50, TileView::getTileDim().y*10}, 32 , 1, BLACK, TileView::getTileDim()*100);
+        drawTextLinesWrapped(View2D::getFont("Kabel"), tileFooter, 
+                             {TileView::getTileDim().x*50, TileView::getTileDim().y*90}, 32 , 1, BLACK, TileView::getTileDim()*80);
+        EndTextureMode();
+        
+        Image textImg = LoadImageFromTexture(text.texture);
+        UnloadTexture(text.texture);
+        //ImageFlipVertical(&textImg);
+        Texture textTexture = LoadTextureFromImage(textImg);
+        Image icon = LoadImage("data/GUIAssets/jail_icon.png");
+        if (icon.height > getTileDim().y*35) {
+            ImageResize(&icon, icon.width*(getTileDim().y*35/icon.height), getTileDim().y*35);
+        }
+        Texture iconTexture = LoadTextureFromImage(icon);
+        UnloadImage(icon);
+        UnloadImage(textImg);
+        BeginTextureMode(tileTexture);
+        DrawTexture(textTexture, 0, 0, WHITE);
+        DrawTexture(iconTexture, (getTileDim().x*100 - iconTexture.width)/2, (getTileDim().y*60 + iconTexture.width)/2, WHITE);
+        EndTextureMode();
+        UnloadTexture(iconTexture);
+        UnloadTexture(textTexture);
+    } else {
+        DrawRectangle(0,getTileDim().y*35, getTileDim().y*65, getTileDim().y*65, ORANGE);
+        DrawRectangleLinesEx({0,getTileDim().y*35, getTileDim().y*65, getTileDim().y*65},6, BLACK);
+        DrawRectangleLinesEx({0,0,getTileDim().y*100, getTileDim().y*100}, 6, BLACK);
+        EndTextureMode();
+        Vector2 textBB = ((Vector2){getTileDim().y*0.8f, getTileDim().y*1.45f})*32.5f*sqrt(2);
+        RenderTexture2D text = LoadRenderTexture(textBB.x, textBB.y);
+        BeginTextureMode(text);
+        float headerSpace = 0.15;
+        drawTextLinesWrapped(View2D::getFont("Kabel"), "IN", 
+                             {textBB.x/2, textBB.y*headerSpace}, 32 , 1, BLACK, textBB);
+        drawTextLinesWrapped(View2D::getFont("Kabel"), "JAIL", 
+                             {textBB.x/2, textBB.y*0.9f}, 32 , 1, BLACK, textBB);
+        EndTextureMode();
+        Image textImg = LoadImageFromTexture(text.texture);
+        UnloadTexture(text.texture);
+        ImageRotate(&textImg, 45);
+        Texture textTexture = LoadTextureFromImage(textImg);
+        UnloadImage(textImg);
+        BeginTextureMode(tileTexture);
+        DrawTexture(textTexture, getTileDim().y*32.5 - textImg.width/2, 
+                    getTileDim().y*67.5 - textImg.height/2, WHITE);
+        
+
+        Image icon = LoadImage("data/GUIAssets/jail_icon.png");
+        ImageFlipVertical(&icon);
+        if (icon.width > textBB.x*0.9) {
+            ImageResize(&icon, textBB.x*0.9, icon.height*(textBB.x*0.9/icon.width));
+        } 
+        ImageRotate(&icon, 45);
+        
+        Texture iconTexture = LoadTextureFromImage(icon);
+        DrawTexture(iconTexture,getTileDim().y*32.5 - icon.width/2, 
+                    getTileDim().y*67.5 - icon.height/2, WHITE);
+        EndTextureMode();
+
+        RenderTexture2D textTexture1 = LoadRenderTexture(getTileDim().y*100, getTileDim().y*100);
+        Vector2 textDim = MeasureTextEx(View2D::getFont("Kabel"), "JUST", 44, 1);
+        BeginTextureMode(textTexture1);
+        drawTextLinesWrapped(View2D::getFont("Kabel"), "JUST", 
+                             {getTileDim().y*40, getTileDim().y*100 - textDim.y}, 44 , 1, BLACK, {getTileDim().y*100, getTileDim().y*100});
+        EndTextureMode();
+        Image textImg1 = LoadImageFromTexture(textTexture1.texture);
+        UnloadTexture(textTexture1.texture);
+        ImageFlipVertical(&textImg1);
+        ImageFlipHorizontal(&textImg1);
+        ImageRotate(&textImg1, 180);
+        Texture textTexture1b = LoadTextureFromImage(textImg1);
+
+        RenderTexture2D textTexture2 = LoadRenderTexture(getTileDim().y*100, getTileDim().y*100);
+        textDim = MeasureTextEx(View2D::getFont("Kabel"), "VISITING", 44, 1);
+        BeginTextureMode(textTexture2);
+        drawTextLinesWrapped(View2D::getFont("Kabel"), "VISITING", 
+                             {getTileDim().y*60, getTileDim().y*100 - textDim.y}, 44 , 1, BLACK, {getTileDim().y*100, getTileDim().y*100});
+        EndTextureMode();
+        Image textImg2 = LoadImageFromTexture(textTexture2.texture);
+        UnloadTexture(textTexture2.texture);
+        ImageFlipVertical(&textImg2);
+        ImageFlipHorizontal(&textImg2);
+        ImageRotate(&textImg2, 270);
+        Texture textTexture2b = LoadTextureFromImage(textImg2);
+
+        BeginTextureMode(tileTexture);
+        DrawTexture(textTexture1b, 0, 0, WHITE);
+        DrawTexture(textTexture2b, 0, 0, WHITE);
+
+        EndTextureMode();
+        UnloadImage(icon);
+        UnloadImage(textImg1);
+        UnloadImage(textImg2);
+        UnloadTexture(iconTexture);
+        UnloadTexture(textTexture);
+        UnloadTexture(textTexture1b);
+        UnloadTexture(textTexture2b);
+    }
+}
